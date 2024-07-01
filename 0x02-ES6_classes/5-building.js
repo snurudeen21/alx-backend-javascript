853d9a41b9819cbedcadb2bc0d1688603974febf
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-07-01 16:08:08 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,33 @@
-export default class Building {
+class Building {
   constructor(sqft) {
-    // if (new.target === Building) {
-    // throw new Error('Can"t instantiate class Building');
-    //    }
+    if (new.target === Building) {
+      throw new Error("Building is an abstract class and cannot be instantiated directly.");
+    }
     this._sqft = sqft;
   }
 
+  // Getter for sqft attribute
   get sqft() {
     return this._sqft;
   }
 
-  /* eslint-disable class-methods-use-this */
+  // Abstract method to be implemented by subclasses
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
-  /* eslint-enable class-methods-use-this */
+}
+
+class Office extends Building {
+  constructor(sqft, name) {
+    super(sqft);
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate the building ${this._name} immediately!';
+  }
 }
