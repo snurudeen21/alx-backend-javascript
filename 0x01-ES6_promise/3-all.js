03d63e984ea2c0aff002c261c67c159a3ae3c927
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-07-01 14:45:34 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([
-    uploadPhoto().catch((error) => {
-      console.error('Error uploading photo:', error);
-      return { body: null }; // Return a default value
-    }),
-    createUser().catch((error) => {
-      console.error('Error creating user:', error);
-      return { firstName: null, lastName: null }; // Return default values
-    }),
-  ])
-    .then(([uploadPhotoResult, createUserResult]) => {
-      console.log(uploadPhotoResult.body, createUserResult.firstName, createUserResult.lastName);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
     })
-    .catch((error) => {
-      console.log('Signup system offline:', error);
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
