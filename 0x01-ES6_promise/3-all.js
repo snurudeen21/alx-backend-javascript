378badbcdeef1ea9d943e5aa4bf151f928a2cb47
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-07-01 13:34:55 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhotoResult, createUserResult]) => {
+    .then((results) => {
+      const [uploadPhotoResult, createUserResult] = results;
       console.log(uploadPhotoResult.body, createUserResult.firstName, createUserResult.lastName);
     })
     .catch(() => {
