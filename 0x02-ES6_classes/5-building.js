b3b130f32b98220668bc7ce50dc188d515ccfbbf
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-07-02 10:31:42 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    // if (new.target === Building) {
-    // throw new Error('Can"t instantiate class Building');
-    //    }
     this._sqft = sqft;
   }
 
@@ -10,9 +7,7 @@ export default class Building {
     return this._sqft;
   }
 
-  /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-  /* eslint-enable class-methods-use-this */
 }
